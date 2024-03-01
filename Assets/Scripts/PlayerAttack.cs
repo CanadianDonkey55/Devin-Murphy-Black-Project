ccafd8df8b683809db2d72e365ef99f5d1ae3fe4
Code-Ninjas-Stouffville <codeninjasstouffville@outlook.com>
2024-02-29 19:01:49 -0500

    lots of stuff

--- Assets/Scripts/PlayerAttack.cs
+++ Assets/Scripts/PlayerAttack.cs
@@ -4,36 +4,34 @@ using UnityEngine;
 
 public class PlayerAttack : MonoBehaviour
 {
+    [Header("Bullet")]
     public GameObject bullet;
     public GameObject bulletLocation;
     BulletMove move;
     public float bulletSpeed;
+
+    [Header("Shoot")]
+    [SerializeField] float startingShootCooldown = 1.5f;
+    float shootCooldown;
 
     GameObject FiredBullet;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        shootCooldown = startingShootCooldown;
     }
 
     // Update is called once per frame
     void Update()
-    {
-
-        if (Input.GetMouseButtonDown(0))
+    { 
+        if (Input.GetMouseButtonDown(0) && shootCooldown <= 0)
         {
             var playerMovement = gameObject.GetComponent<PlayerMovement>();
             FiredBullet = Instantiate(bullet, bulletLocation.transform.position, playerMovement.bullet.transform.localRotation);
-           // Vector3 dir = new Vector3(playerMovement.InputMax, playerMovement.InputMax);
-
-            
-
-           // FiredBullet.transform.localRotation = Quaternion.Euler(0, 0, 90);
-            
             FiredBullet.GetComponent<BulletMove>().playerDirection = new Vector2(playerMovement.InputMax, 0);
-
-           
+            shootCooldown = startingShootCooldown;
         }
+        shootCooldown -= Time.deltaTime;
     }
 }
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -20,6 +20,10 @@ public class PlayerMovement : MonoBehaviour
     public Sprite front;
     public Sprite left;
     public Sprite right;
+    public Sprite backRight;
+    public Sprite backLeft;
+    public Sprite gun;
+    public Sprite gunBackRight;
 
     // Start is called before the first frame update
     void Start()
@@ -48,48 +52,80 @@ public class PlayerMovement : MonoBehaviour
         if (horizontalInput > 0)
         {
             InputMax = 1;
-            Gun.transform.localRotation = Quaternion.Euler(0, 0, 0);
-            bullet.transform.rotation = Quaternion.Euler(0, 0, 0);
+            Directional(right, new Vector3(0, 0, 0), new Vector3(0, 0, 0), false, new Vector3(0.281f, -0.063f, 0));
+            //Gun.transform.localRotation = Quaternion.Euler(0, 0, 0);
+           // bullet.transform.rotation = Quaternion.Euler(0, 0, 0);
             Gun.transform.localScale = new Vector3(Mathf.Abs(Gun.transform.localScale.x), Gun.transform.localScale.y, Gun.transform.localScale.z);
-            Gun.GetComponent<SpriteRenderer>().flipY = false;
-            renderer.sprite = right;
-            Gun.transform.localPosition = new Vector3(0.281f, -0.063f, 0);
+            //Gun.GetComponent<SpriteRenderer>().flipY = false;
+           // renderer.sprite = right;
+           // Gun.transform.localPosition = new Vector3(0.281f, -0.063f, 0);
             Gun.GetComponent<SpriteRenderer>().sortingOrder = 101;
         }
         else if (horizontalInput < 0)
         {
             InputMax = -1;
-            Gun.transform.localRotation = Quaternion.Euler(0, 0, 0);
-            bullet.transform.rotation = Quaternion.Euler(0, 0, 0);
+            Directional(left, new Vector3(0, 0, 0), new Vector3(0, 0, 0), false, new Vector3(-0.281f, -0.063f, 0));
+           // Gun.transform.localRotation = Quaternion.Euler(0, 0, 0);
+           // bullet.transform.rotation = Quaternion.Euler(0, 0, 0);
             Gun.transform.localScale = new Vector3(-Mathf.Abs(Gun.transform.localScale.x), Gun.transform.localScale.y, Gun.transform.localScale.z);
-            Gun.GetComponent<SpriteRenderer>().flipY = false;
-            renderer.sprite = left;
-            Gun.transform.localPosition = new Vector3(-0.281f, -0.063f, 0);
+           // Gun.GetComponent<SpriteRenderer>().flipY = false;
+           // renderer.sprite = left;
+           // Gun.transform.localPosition = new Vector3(-0.281f, -0.063f, 0);
             Gun.GetComponent<SpriteRenderer>().sortingOrder = 99;
         }
 
         if (verticalInput > 0)
         {
             InputMax = 1;
-            Gun.transform.localRotation = Quaternion.Euler(0, 0, 90);
-            bullet.transform.rotation = Quaternion.Euler(0, 0, 90);
+            Directional(back, new Vector3(0, 0, 90), new Vector3(0, 0, 90), true, new Vector3(0.433f, 0.031f, 0));
+            //Gun.transform.localRotation = Quaternion.Euler(0, 0, 90);
+            //bullet.transform.rotation = Quaternion.Euler(0, 0, 90);
             Gun.transform.localScale = new Vector3(Mathf.Abs(Gun.transform.localScale.x), Gun.transform.localScale.y, Gun.transform.localScale.z);
-            Gun.GetComponent<SpriteRenderer>().flipY = true;
-            renderer.sprite = back;
-            Gun.transform.localPosition = new Vector3(0.433f, 0.031f, 0);
+           // Gun.GetComponent<SpriteRenderer>().flipY = true;
+            //renderer.sprite = back;
+            //Gun.transform.localPosition = new Vector3(0.433f, 0.031f, 0);
             Gun.GetComponent<SpriteRenderer>().sortingOrder = 99;
         }
         else if (verticalInput < 0)
         {
             InputMax = -1;
-            Gun.transform.localRotation = Quaternion.Euler(0, 0, -90);
-            bullet.transform.rotation = Quaternion.Euler(0, 0, 90);
+            Directional(front, new Vector3(0, 0, -90), new Vector3(0, 0, 90), true, new Vector3(-0.375f, -0.3f, 0));
+           // Gun.transform.localRotation = Quaternion.Euler(0, 0, -90);
+            //bullet.transform.rotation = Quaternion.Euler(0, 0, 90);
             Gun.transform.localScale = new Vector3(Mathf.Abs(Gun.transform.localScale.x), Gun.transform.localScale.y, Gun.transform.localScale.z);
-            Gun.GetComponent<SpriteRenderer>().flipY = true;
-            renderer.sprite = front;
-            Gun.transform.localPosition = new Vector3(-0.375f, -0.3f, 0);
+           // Gun.GetComponent<SpriteRenderer>().flipY = true;
+           // renderer.sprite = front;
+           // Gun.transform.localPosition = new Vector3(-0.375f, -0.3f, 0);
             Gun.GetComponent<SpriteRenderer>().sortingOrder = 101;
         }
+
+        if (horizontalInput > 0 && verticalInput > 0)
+        {
+            Directional(backRight, new Vector3(0, 0, 45), new Vector3(0, 0, 45), false, new Vector3(0.315f, -0.05f, 0));
+            //Gun.transform.localRotation = Quaternion.Euler(0, 0, 45);
+            //bullet.transform.rotation = Quaternion.Euler(0, 0, 45);
+            //Gun.GetComponent<SpriteRenderer>().flipY = false;
+            //renderer.sprite = backRight;
+            Gun.GetComponent<SpriteRenderer>().sortingOrder = 101;
+        }
+        if (horizontalInput < 0 && verticalInput > 0)
+        {
+            Directional(backLeft, new Vector3(0, 0, 135), new Vector3(0, 0, 135), true, new Vector3(0.289f, 0.006f, 0));
+            //Gun.transform.localRotation = Quaternion.Euler(0, 0, 135);
+           // bullet.transform.rotation = Quaternion.Euler(0, 0, 135);
+           // Gun.GetComponent<SpriteRenderer>().flipY = true;
+           //renderer.sprite = backLeft;
+            Gun.GetComponent<SpriteRenderer>().sortingOrder = 99;
+        }
+    }
+
+    void Directional(Sprite characterSprite, Vector3 gunRot, Vector3 bulletRot, bool gunYFlip, Vector3 gunPos)
+    {
+        Gun.transform.localRotation = Quaternion.Euler(gunRot);
+        bullet.transform.rotation = Quaternion.Euler(bulletRot);
+        Gun.GetComponent<SpriteRenderer>().flipY = gunYFlip;
+        renderer.sprite = characterSprite;
+        Gun.transform.localPosition = gunPos;
     }
 
     
